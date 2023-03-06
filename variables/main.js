650a7292832c2b30d0c01aff0e4cd8a65a99f22a
MondoBazoo <isaiahrivera850@gmail.com>
2023-03-06 11:43:36 -0800

    add: variable solutions

---
+++ variables/main.js
@@ -0,0 +1,17 @@
+//Working with variables:
+//1. Declare two variables: admin and someName.
+let admin;
+let someName;
+//2. Assign the value "John" to someName.
+someName = "John";
+//3. Copy the value from someName to admin.
+admin = someName;
+//4. Show the value of admin using alert (must output “John”).
+alert(admin);
+
+//Giving the right name: 
+//1. Create a variable with the name of our planet. How would you name such a variable?
+let ourPlanetName = 'Earth';
+//2. Create a variable to store the name of a current visitor to a website. How would you name that variable?
+let currentVisitor = 'Finn';
+
